62094a57087bba8d458eed7e07d8dc2cceeffcf8
ArabeZAP <77598136+ArabeZAP@users.noreply.github.com>
2021-02-07 16:10:26 -0300

    Add files via upload

---
+++ database/menu/imagemaker.js
@@ -0,0 +1,58 @@
+const imagemaker = (prefix, botName, ownerName) => {
+        return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+░░░░░░░░░▄█████████▄▄▄░░░░░░░
+░░░░░░░░▐███████████████▄▄░░░
+░░░░░░░░███████████████████▄░
+░░░░░░░█████████████████████▌
+░░░░░░▐█▀▀▀▀██▀▒▒▒▒▒▒▒▒▒▀███░
+░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░
+░░░░░▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌░░
+░░░▄▀▀▀▀▄▒▒▒▒▒▀▀▀▄▄▒▒▒▒▒▒▒▐░░
+░░▐▒▄▀▀▒▒▒▒▒▒▄▄▄▄▄▒▒▒▒▒▀▄▄▐░░
+░░▐▒▒▒▀▒▒▒▒▒▒▒▌▀▒▌▒▒▒▀▄▄▄▒▐░░
+░░░▀▄▒▄▒▒▒▒▒▒▒▒▀▀▒▒▒▌▒▌▀▐▒▌░░
+░░░░▐▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐▒▒▀▒▐░░░
+░░░░▌▒▒▒▒▒▒▒▒▒▒▒▒▀▄▄▒▀▒▒▒▌░░░
+░░░▐▒▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▐░░░░
+░░▄▌▒▒▒▒▒▒▒▒▒▒▒██████▄▒▐░░░░░
+░█▐▒▒▒▒▒▀▒▒▒▒▒▒▒▀▀██▀▒▐░░░░░░
+▀▒▐▒▒▒▒▒▒▀▒▒▒▒▒▒▒▒▒▒▒▐░░▄▄░░░
+░░░▐▒▒▒▒▒▒▒▀▄▒▒▒▒▒▒▒▐░░▐░░▐░░
+░░░░▐▒▒▒▒▒▒▒▒▀▄▄▄▄▄▀░▄▄▐░░▐▄▄
+░░░░░▐▒▒▒▒▒▒▒▒▒▌▌░░░▐░░▐░░▐░▐
+░░░░░▐▒▒▒▒▒▒▒▐▐ ░░░░▐░░░░░░░░
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *CRIAR IMAGENS - OFF*
+  │
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}herrypotter
+  └─ ❏ ${prefix}quotemaker`
+}
+exports.imagemaker = imagemaker
